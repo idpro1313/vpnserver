eb12ce7992929b047a58d41ec60ef7f97dc3d256
idpro <126682172+idpro1313@users.noreply.github.com>
2024-11-07 20:59:09 +0300

    Add files via upload

---
+++ install_all_components.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Установка Ansible
+sudo apt update
+sudo apt install -y ansible
+
+# Проверка и установка обновлений для Ubuntu
+sudo apt update
+sudo apt upgrade -y
+
+# Создание и запуск Playbooks для установки компонентов
+cat <<EOF > base.yml
+---
+- name: Setup base components
+  hosts: localhost
+  tasks:
+    - name: Install required base components
+      apt:
+        name: "{{ item }}"
+        state: present
+      with_items:
+        - net-tools
+        - mc
+        - nano
+        - htop
+        - git
+        - cron
+        - curl
+EOF
+
+cat <<EOF > docker.yml
+---
+- name: Setup Docker components
+  hosts: localhost
+  tasks:
+    - name: Install Docker
+      apt:
+        name: docker-ce
+        update_cache: yes
+
+    - name: Install Docker Compose
+      shell: curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
+
+    - name: Install Portainer
+      docker_container:
+        name: portainer
+        image: portainer/portainer
+        restart_policy: unless-stopped
+        ports:
+          - "9000:9000"
+EOF
+
+cat <<EOF > vpn.yml
+---
+- name: Setup Wireguard VPN
+  hosts: localhost
+  tasks:
+    - name: Install Wireguard and related components
+      apt:
+        name: "{{ item }}"
+        state: present
+      with_items:
+        - wireguard
+        - wireguard-tools
+EOF
+
+# Запуск Playbooks с помощью Ansible
+ansible-playbook base.yml
+ansible-playbook docker.yml
+ansible-playbook vpn.yml
